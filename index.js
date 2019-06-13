c4c6c95ce6e6dfd3175d1d9e424ba85b1de57dca
Georgeta Pojoga <georgeta.pojoga@tecnicosuperiorekennedy.it>
2019-06-13 12:47:44 +0200

    update

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.post('/', function (req, res) {
+    res.send('Got a POST request')
+  })
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`)) 
